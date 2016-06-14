45c51f4a4529140fccc46af52ce4797039c19559
cesare rocchetti <cesare@cr-consult.eu>
2016-06-14 11:36:43 +0200

    Aggiungere i file di progetto.

---
+++ NCProgramMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCProgramMVC.MvcApplication" Language="C#" %>
---
+++ NCProgramMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCProgramMVC.Startup))]
+namespace NCProgramMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
